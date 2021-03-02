5f87914f4adafc440f538f81cfe53918a4b62f38
Penumalla Ramyatha <ramyathapenumalla27@gmail.com>
2021-03-02 11:00:50 +0530

    full project

---
+++ crudtask2/Models/DeptMeta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace crudtask2.Models
+{
+    public class DeptMeta
+    {
+        [Required]
+        [Display(Name ="Department Id")]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Department Name")]
+        public int Name { get; set; }
+        [Required]
+        [Display(Name = "Location")]
+        public int Location { get; set; }
+
+
+    }
+    [MetadataType(typeof(DeptMeta))]
+    public partial class Dept
+    {
+
+    }
+       
+
+}
---
+++ crudtask2/Models/Studentmeta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace crudtask2.Models
+{
+    public class Studentmeta
+    {
+        [Display(Name = " Id")]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Mobile Number")]
+        public string PhNo { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Display(Name = "Birthdate")]
+        [DataType(DataType.Date)]
+        
+        public Nullable<System.DateTime>  Dob { get; set; }
+    [Display(Name = "Address")]
+    public string Address { get; set; }
+    [Display(Name = "Gender")]
+    public string Gender { get; set; }
+        [Display(Name = "Dept Name")]
+        public Nullable<int> DeptId { get; set; }
+        public virtual depart depart { get; set; }
+    }
+    [MetadataType(typeof(Studentmeta))]
+    public partial class student { }
+}
